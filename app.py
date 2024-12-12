316b5efa55514caf0f6b724d0bcd4e307e7b4bb9
jcowdells <jcowdells@gmail.com>
2024-12-12 14:48:57 +0000

    Added some html and item search

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
 from log.log import *
 from sqsanta.navbar import Navbar
+from db.manager import search_inventory
 
 app = Flask(__name__)
 
@@ -11,16 +12,19 @@ navbar = Navbar(
 )
 
 def render_base(template, **kwargs):
-    return render_template(template, navbar=navbar, **kwargs)
+    return render_template(template, navbar=navbar, title="SQSanta.net", **kwargs)
 
 @app.route("/")
 @app.route("/index/")
 def index():
     return render_base("index.html")
 
-@app.route("/search/")
+@app.route("/search/", methods=["GET", "POST"])
 def search():
-    return render_base("search.html")
+    results = list()
+    if request.method == "POST":
+        results = search_inventory(request.form["search"])
+    return render_base("search.html", results=results)
 
 if __name__ == '__main__':
     app.run()
